02b1d268bfd515169a10ba7f8b5d720ea4ef5f8e
gloria <gloria.gao115@gmail.com>
2021-11-23 21:58:57 +1100

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName ='bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+};
+// upperCaseCustomerName ()
+// console.log(customerName);
+
+
+function setBestCustomer (){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+};
+// setBestCustomer();
+// console.log(bestCustomer);
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+} 
+// overwriteBestCustomer();
+// console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'abc';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'eft';
+}
+console.log(leastFavoriteCustomer);
